838fbef5a1ca52bb11f09672948426b449a8d906
Monisha White <mewhite@stanford.edu>
2014-09-10 18:15:57 -0700

    Still working on figuring out UIViews, UIViewControllers, and getting objects/buttons to show up on views/the correct ones.

--- PostureCurveView/ViewController.swift
+++ PostureCurveView/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+
+    var myPostureCurveView = PostureCurveView(frame: CGRectMake(30, 30, 250, 250))
+    
+
+    @IBOutlet weak var angle1: UITextField!
+    @IBOutlet weak var angle2: UITextField!
+    @IBOutlet weak var angle3: UITextField!
+    @IBOutlet weak var angle4: UITextField!
+    @IBOutlet weak var angle5: UITextField!
+    @IBOutlet weak var angle6: UITextField!
+    @IBOutlet weak var accel1: UITextField!
+    @IBOutlet weak var accel2: UITextField!
+    
+    @IBAction func angleDidChange(sender: AnyObject)
+    {
+        myPostureCurveView.angleChange(angle1.text.toInt()!, angle2: angle2.text.toInt()!, angle3: angle3.text.toInt()!, angle4: angle4.text.toInt()!, angle5: angle5.text.toInt()!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    func setupView()
+    {
+        view.addSubview(myPostureCurveView)
+        
     }
 
     override func didReceiveMemoryWarning() {
